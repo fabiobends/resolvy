79e6bd8f67afb7d8d250121a90aa84236ce2e0bb
Fábio Mendes <fabiobends@gmail.com>
2026-05-25 13:16:15 -0300

    feat(storybook): include features stories in config
    - Add src/features/**/stories glob to main.ts
    - Update storybook.requires.ts with features context

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,10 @@
 import { StorybookConfig } from "@storybook/react-native";
 
 const config: StorybookConfig = {
-  stories: ["../src/components/**/stories.?(ts|tsx|js|jsx)"],
+  stories: [
+    "../src/components/**/stories.?(ts|tsx|js|jsx)",
+    "../src/features/**/stories.?(ts|tsx|js|jsx)",
+  ],
   deviceAddons: [
     "@storybook/addon-ondevice-controls",
     "@storybook/addon-ondevice-actions",
